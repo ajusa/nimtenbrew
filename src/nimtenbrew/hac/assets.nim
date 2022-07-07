8329730342353500df30bea3be37c1d8cded655b
TurtleP <jpostelnek@outlook.com>
2022-07-07 00:33:14 -0400

    more testing stuff

--- src/nimtenbrew/hac/assets.nim
+++ src/nimtenbrew/hac/assets.nim
@@ -20,3 +20,31 @@ struct(*assetsHeader, endian = l):
 const ASSETS_HEADER_SIZE*: uint = 0x38
 
 export toAssetsHeader
+
+proc setIcon*(assets: var AssetsHeader, nroSize: uint, size: uint) =
+    ## Set the icon AssetSection information for the NRO
+
+    assets.icon.offset = ASSETS_HEADER_SIZE
+    assets.icon.size = size.uint64
+
+proc setNacp*(assets: var AssetsHeader, nroSize: uint, size: uint,
+        offset: var int = -1) =
+    ## Set the NACP AssetSection information for the NRO
+    ## `offset` should be relative to the assetsHeaderSize + iconSize
+
+    if offset == -1:
+        offset = assets.nacp.offset.int
+
+    assets.nacp.offset = offset.uint64
+    assets.nacp.size = size.uint64
+
+proc setRomfs*(assets: var AssetsHeader, nroSize: uint, size: uint,
+        offset: var int = -1) =
+    ## Set the RomFS AssetSection information for the NRO
+    ## `offset` should be relative to the assetsHeaderSize + iconSize + nacpSize
+
+    if offset == -1:
+        offset = assets.romfs.offset.int
+
+    assets.romfs.offset = offset.uint64
+    assets.romfs.size = size.uint64
