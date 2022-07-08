443ba83b5154604181a155fc674b4e9ae20a8313
TurtleP <jpostelnek@outlook.com>
2022-07-07 20:38:04 -0400

    remove icon test I suppose

--- tests/t_ctr_icon.nim
+++
@@ -1,8 +0,0 @@
-import nimtenbrew/ctr/icon
-
-let png = icon.load("./tests/data/ctr/test.png", 48)
-let data = icon.convertPNGToIcon(png)
-
-let file = io.open("test.dat", fmWrite)
-file.write(data)
-file.close()
